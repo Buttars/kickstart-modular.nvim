d3ee44e94ff415c7cfc32888c7b90f90eba949fc
Landon <=>
2025-05-07 17:45:58 -0600

    fix: deconflict deno_ls and ts_ls for typescript projects

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -215,7 +215,16 @@ return {
         denols = {},
         gopls = {},
         html = {},
-        ts_ls = {},
+        ts_ls = {
+          root_dir = require('lspconfig').util.root_pattern { 'package.json', 'tsconfig.json' },
+          single_file_support = false,
+          settings = {},
+        },
+        denols = {
+          root_dir = require('lspconfig').util.root_pattern { 'deno.json', 'deno.jsonc' },
+          single_file_support = false,
+          settings = {},
+        },
         yamlls = {},
 
         -- clangd = {},
