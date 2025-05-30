802a4763f48004ad64c7462b481685eb8829a5fc
Landon <=>
2025-05-29 19:31:22 -0600

    feat: configure copilot plugin

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,7 +4,15 @@ return {
   event = 'InsertEnter', -- or use a more eager trigger like 'VeryLazy'
   opts = {
     suggestion = {
-      enabled = false, -- disable inline suggestions (optional if using Avante)
+      enabled = true, -- Enable inline suggestions
+      auto_trigger = true, -- Automatically trigger suggestions as you type
+      debounce = 75, -- Debounce time for suggestions
+      keymap = {
+        accept = '<C-l>', -- Keybinding to accept the suggestion
+        -- next  aa= '<C-]>',
+        -- prev = '<C-[>',
+        dismiss = '<C-\\>',
+      },
     },
     panel = {
       enabled = false, -- disable Copilot panel (optional if using Avante)
@@ -19,5 +27,17 @@ return {
   },
   config = function(_, opts)
     require('copilot').setup(opts)
+
+    -- Extend nvim-cmp configuration to handle Copilot suggestions
+    local cmp = require 'cmp'
+
+    -- Add event handlers for menu_opened and menu_closed
+    cmp.event:on('menu_opened', function()
+      vim.b.copilot_suggestion_hidden = true
+    end)
+
+    cmp.event:on('menu_closed', function()
+      vim.b.copilot_suggestion_hidden = false
+    end)
   end,
 }
